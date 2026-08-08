ac265c662a7c7464b3757b03862baf65c155af46
Samuel Ayer <ayersamuel07@gmail.com>
2026-08-08 06:58:37 +0000

    Initial commit of great expectations set up for data validation

---
+++ include/gx/setup_gx.py
@@ -0,0 +1,131 @@
+import great_expectations as gx
+from pathlib import Path
+
+
+def create_daily_climate_pre_load_suite(context):
+    suite_name = "pre_load_climate"
+    pre_load_climate_suite = gx.ExpectationSuite(name=suite_name)
+    pre_load_climate_suite = context.suites.add_or_update(pre_load_climate_suite)
+
+    pre_load_climate_suite.add_expectation(gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A='temperature_2m_max',
+        column_B='temperature_2m_mean',
+        or_equal=True,
+        mostly=1.0
+    ))
+
+    pre_load_climate_suite.add_expectation(gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A='temperature_2m_mean',
+        column_B='temperature_2m_min',
+        or_equal=True,
+        mostly=1.0
+    ))
+
+    pre_load_climate_suite.add_expectation(gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+        column_A='relative_humidity_2m_max',
+        column_B='relative_humidity_2m_mean',
+        or_equal=True,
+        mostly=1.0
+    ))
+
+    pre_load_climate_suite.add_expectation(gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+            column_A='relative_humidity_2m_mean',
+            column_B='relative_humidity_2m_min',
+            or_equal=True,
+            mostly=1.0
+        ))    
+
+    percentage_cols = ['relative_humidity_2m_max', 'relative_humidity_2m_min', 'relative_humidity_2m_mean', 'cloud_cover_mean']
+    for percentage_col in percentage_cols:
+        pre_load_climate_suite.add_expectation(gx.expectations.ExpectColumnValuesToBeBetween(
+            column=percentage_col,
+            min_value=0,
+            max_value=100,
+            mostly=1.0
+        ))
+
+    pre_load_climate_suite.add_expectation(gx.expectations.ExpectColumnValuesToBeBetween(
+        column='soil_moisture_0_to_10cm_mean',
+        min_value=0,
+        max_value=1,
+        mostly=1.0
+    ))
+
+    non_negative_cols = ['wind_speed_10m_mean', 'wind_speed_10m_max', 'shortwave_radiation_sum', 'soil_moisture_0_to_10cm_mean', 'precipitation_sum',
+                         'rain_sum', 'snowfall_sum']
+    for non_negative_col in non_negative_cols:
+        pre_load_climate_suite.add_expectation(gx.expectations.ExpectColumnValuesToBeBetween(
+            column=non_negative_col,
+            min_value=0,
+            max_value=None,
+            mostly=1.0
+        ))
+
+
+def create_daily_air_quality_pre_load_suite(context):
+    suite_name = "pre_load_air_quality"
+    pre_load_air_quality_suite = gx.ExpectationSuite(name=suite_name)
+    pre_load_air_quality_suite =  context.suites.add_or_update(pre_load_air_quality_suite)
+
+    air_quality_cols = ['pm2_5_mean', 'pm10_mean', 'nitrogen_dioxide_1h_max', 'sulphur_dioxide_1h_max', 'ozone_8h_max', 'carbon_dioxide_mean', 'carbon_monoxide_8h_max']
+    for air_quality_col in air_quality_cols:
+        pre_load_air_quality_suite.add_expectation(gx.expectations.ExpectColumnValuesToBeBetween(
+            column=air_quality_col,
+            min_value=0,
+            max_value=None,
+            mostly=1.0
+        ))
+
+def configure_checkpoint(context, api_source):
+
+    expectation_suite = context.suites.get(name=f"pre_load_{api_source}")
+
+    data_source = context.data_sources.add_or_update_pandas_filesystem(
+        name=f"{api_source} parquet file",
+        base_directory=Path(f'/opt/airflow/include/data/transformed/daily_{api_source}')
+    )
+
+    parquet_asset = data_source.add_parquet_asset(
+        name=f"{api_source}_raw_parquet"
+    )
+
+    batch_definition = parquet_asset.add_batch_definition_daily(
+        name=f"daily_{api_source}_batch",
+        regex=(
+            r"(?P<year>\d{4})-"
+            r"(?P<month>\d{2})-"
+            r"(?P<day>\d{2})"
+            r"\.parquet"
+        )
+    )
+
+    validation_definition = context.validation_definitions.add_or_update(
+        gx.ValidationDefinition(
+            name=f"daily_{api_source}_validation",
+            data=batch_definition,
+            suite=expectation_suite
+        )
+    )
+
+    checkpoint = gx.Checkpoint(
+            name=f"daily_{api_source}_checkpoint",
+            validation_definitions=[validation_definition],
+            actions=[]
+        )
+    
+    return context.checkpoints.add_or_update(checkpoint)
+
+if __name__ == '__main__':
+    include_root = Path(__file__).resolve().parents[1]
+
+    GX_ROOT = include_root / "gx"
+
+    context = gx.get_context(project_root_dir=GX_ROOT)
+
+    create_daily_climate_pre_load_suite(context)
+
+    create_daily_air_quality_pre_load_suite(context)
+
+    sources = ['climate', 'air_quality']
+    for source in sources:
+        configure_checkpoint(context, source)
