f28910e38351670253ac04be2dd0c6b9991f0de3
Samuel Ayer <ayersamuel07@gmail.com>
2026-08-25 08:56:50 +0000

    feat: make operator aware of succeeded paths when deferring other path tasks

--- dags/utils/custom/operators.py
+++ dags/utils/custom/operators.py
@@ -1,5 +1,4 @@
-from typing import Any
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from airflow.sdk import BaseOperator, Context
 from airflow.providers.standard.triggers.temporal import TimeDeltaTrigger
@@ -13,32 +12,58 @@ class QuotaAwareOpenMeteoExtractionOperator(BaseOperator):
         self.period = period
         self.parquet_paths = parquet_paths
 
-    def execute(self, context: Context) -> None:
-        try:
-            return self._run_extraction(context)
-        except Exception as e:
-            error = str(e)
-            if "hourly" in error.lower():
-                self.log.warning(f"Hourly API quota exhausted. Deferring task for 1 day.")
+    def execute(self, context: Context, current_path_index = 0, completed_paths=None, event=None):
+        completed_paths = completed_paths or []
+
+        for i in range(current_path_index, len(self.parquet_paths)):
+            parquet_path = self.parquet_paths[i]
+        
+            try:
+                result = self.python_callable(period=self.period, cities_chunk_path=parquet_path)
+                completed_paths.append(result)
+            except Exception as error:
+                if isinstance(error, dict):
+                    reason = error.get("reason", "").lower()
+                else:
+                    reason = str(error).lower()
+
+                if "minutely api request limit exceeded" in reason:
+                    self.log.warning("Minutely API quota exhausted. Deferring task for 1 day.")
+                    delay = timedelta(minutes=1)
+                elif "hourly api request limit exceeded" in reason:
+                    self.log.warning(f"Hourly API quota exhausted. Deferring task for 1 day.")
+                    delay = timedelta(hours=1)
+                    
+                elif 'daily api request limit exceeded' in reason:
+                    now = datetime.now()
+                    next_run = (now + timedelta(days=1)).replace(hour=0, minute=30)
+                    delay = next_run - now
+                    self.log.warning(f"Daily API quota exhausted. Deferring task for {delay} hours.")
+                    # delay = timedelta(days=1)
+
                 self.defer(
-                    trigger=TimeDeltaTrigger(timedelta(hours=1)),
-                    method_name='execute_complete'
+                trigger=TimeDeltaTrigger(delay),
+                method_name='execute',
+                kwargs={
+                    "current_path_index": current_path_index,
+                    "completed_paths": completed_paths
+                }
                 )
-            elif 'daily' in error.lower():
-                self.log.warning(f"Daily API quota exhausted. Deferring task for 1 day.")
-                self.defer(
-                    trigger=TimeDeltaTrigger(timedelta(days=1)),
-                    method_name='execute_complete'
-                )
-            else:
                 raise AirflowException(error)
+        self.log.info("Returned value: %s", completed_paths)
+        return  completed_paths
 
-    def execute_complete(self, context: Context, event=None):
-        self.log.info("Quota window reset. Resuming extraction.")
-        return self.execute(context)
 
-    def _run_extraction(self,context: Context):
-        return self.python_callable(
-            period=self.period,
-            parquet_chunk_path=self.parquet_paths,
-            **context)
+
+    # def execute_complete(self, context: Context, event=None):
+    #     self.log.info("Quota window reset. Resuming extraction.")
+    #     return self.execute(context)
+
+    # def _run_extraction(self,context: Context, current_path_index, completed_paths: list):
+    #     for i in range(current_path_index, len(self.parquet_paths)):
+    #         parquet_path = self.parquet_paths[i]
+    #         result = self.python_callable(period=self.period, cities_chunk_path=parquet_path)
+
+    #         completed_paths.append(result)
+    #     self.log.info("Returned value: %s", completed_paths)
+    #     return  completed_paths
