c706c9394cf6013ba8c9ed5fd37ae178f3ec4c75
Samuel Ayer <ayersamuel07@gmail.com>
2026-08-29 16:29:36 +0000

    Unit test for historical backfill dag

---
+++ tests/dags/historical_backfill_test.py
@@ -0,0 +1,42 @@
+import pytest
+from airflow.dag_processing.dagbag import DagBag
+
+@pytest.fixture()
+def dagbag():
+    return DagBag(dag_folder="/opt/airflow/dags")
+
+def test_dag_loaded(dagbag):
+    dag = dagbag.get_dag(dag_id='historical_backfill')
+    assert dagbag.import_errors == {}
+    assert dag is not None
+    assert len(dag.tasks) == 13
+
+def assert_dag_dict_equal(source, dag):
+    assert dag.task_dict.keys() == source.keys()
+    for task_id, downstream_list in source.items():
+        assert dag.has_task(task_id)
+        task = dag.get_task(task_id)
+        assert task.downstream_task_ids == set(downstream_list)
+
+
+def test_dag(dagbag):
+    dag = dagbag.get_dag(dag_id='historical_backfill')
+    assert_dag_dict_equal(
+        {
+            "build_city_period_pairs": ['climate_period_pipeline.backfill_climate'],
+            "climate_period_pipeline.backfill_climate": ['climate_period_pipeline.consolidate_daily_climate_chunks'],
+            "climate_period_pipeline.consolidate_daily_climate_chunks": ['climate_period_pipeline.validate_climate_pre_load'],
+            "climate_period_pipeline.emit_climate_year_processed": [],
+            "climate_period_pipeline.upsert_climate": ['climate_period_pipeline.emit_climate_year_processed'],
+            "climate_period_pipeline.validate_climate_pre_load": ['climate_period_pipeline.upsert_climate'],
+            "get_cities": ['build_city_period_pairs', 'land_surface_period_pipeline.backfill_period_land_surface'],
+            "get_periods": ['build_city_period_pairs', 'land_surface_period_pipeline.backfill_period_land_surface'],
+            "land_surface_period_pipeline.backfill_period_land_surface": ['land_surface_period_pipeline.consolidate_daily_land_surface'],
+            "land_surface_period_pipeline.consolidate_daily_land_surface": ['land_surface_period_pipeline.validate_land_surface_pre_load'],
+            "land_surface_period_pipeline.emit_land_surface_year_processed": [],
+            "land_surface_period_pipeline.upsert_land_surface": ['land_surface_period_pipeline.emit_land_surface_year_processed'],
+            "land_surface_period_pipeline.validate_land_surface_pre_load": ['land_surface_period_pipeline.upsert_land_surface'],
+        },
+        dag,
+    )
+
