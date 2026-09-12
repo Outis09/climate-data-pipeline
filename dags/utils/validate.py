b660877d06cc319b535992136b121b356df33615
Samuel Ayer <ayersamuel07@gmail.com>
2026-09-12 13:25:00 +0000

    refactor: get context once and use path to get year, month, day

--- dags/utils/validate.py
+++ dags/utils/validate.py
@@ -4,39 +4,42 @@ from datetime import datetime
 from airflow.sdk.exceptions import AirflowException
 import os
 from cloudpathlib import GSPath
+from pathlib import PurePath
 
-def run_validation(api_source, parquet_path):
+def get_date_from_path(parquet_path):
+    path = PurePath(parquet_path)
+    day = int(path.stem)
+    month = int(path.parent.name)
+    year = int(path.parent.parent.name)
+
+    return year, month, day
+
+def run_validation(api_source, parquet_paths):
     storage_type = os.getenv('STORAGE_BACKEND')
     if storage_type == 'local':
         gx_root = '/opt/airflow/include/gx'
-        df = pd.read_parquet(parquet_path, engine='pyarrow')
+
     else:
         # bucket = os.getenv('BUCKET_NAME')
         gx_root = f'/home/airflow/gcs/data/gx'
-        df = pd.read_parquet(GSPath(parquet_path), engine='pyarrow')
     gx_context = gx.get_context(project_root_dir=gx_root)
-
-    # df = pd.read_parquet(parquet_path, engine='pyarrow')
-    run_date = df['date'].iloc[0]
-    try:
-        run_date = datetime.strptime(run_date, '%Y-%m-%d')
-    except TypeError:
-        pass
-    year = run_date.year # str(run_date.year)
-    month = run_date.month # str(run_date.strftime("%m"))
-    day =  run_date.day #str(run_date.strftime("%d"))
-
     checkpoint = gx_context.checkpoints.get(f"daily_{api_source}_checkpoint") 
-    daily_batch_parameters = {"year":year,
-                                "month":month,
-                                "day":day}
-    result = checkpoint.run(batch_parameters=daily_batch_parameters)
-    validation_result_id = list(result.run_results.keys())[0]
-    validation_result = result.run_results[validation_result_id]
 
-    if validation_result.get_max_severity_failure() == "CRITICAL": 
-    # if not result.success:
-        raise AirflowException(
-            f"{api_source} data failed GX validation for {run_date}"
-        )
-    return parquet_path
+    validated_paths = []
+
+    for parquet_path in parquet_paths:
+        year, month, day = get_date_from_path(parquet_path)    
+        daily_batch_parameters = {"year":f"{year:04d}",
+                                    "month":f"{month:02d}",
+                                    "day":f"{day:02d}"}
+        result = checkpoint.run(batch_parameters=daily_batch_parameters)
+        validation_result_id = list(result.run_results.keys())[0]
+        validation_result = result.run_results[validation_result_id]
+
+        if validation_result.get_max_severity_failure() == "CRITICAL": 
+        # if not result.success:
+            raise AirflowException(
+                f"{api_source} data failed GX validation for {year}/{month}/{day:02d}"
+            )
+        validated_paths.append(parquet_path)
+    return validated_paths
